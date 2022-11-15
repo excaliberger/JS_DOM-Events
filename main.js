307c6cde5bd01b127bf3c86a9dee694b1bae28f2
Emily Berger <excaliberger@gmail.com>
2022-11-15 17:36:27 -0500

    update

--- main.js
+++ main.js
@@ -1,47 +1,83 @@
 console.log("Script attached");
 /*----------- Exercise #1: SELECTING/MANIPULATING ELEMENTS -----------*/
 
-// Select Node #1 and change the text to: "I used the getElementById("node1") method to access this"
+document.getElementById("node1").textContent = 'I used the getElementById("node1") method to access this';
+document.getElementsByClassName("node2")[0].textContent = 'I used the getElementByClassName("node2") method to access this';
 
-// Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
+const byTagName = document.getElementsByTagName("h3");
 
-// Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
+for (let eleH3 of byTagName ) {
+    eleH3.textContent = 'I used the getElementByTagName("h3") method to access all of these';
+}
 
 /*----------- Exercise #2: CREATING/APPENDING/INSERTING ELEMENTS/OBJECTS -----------*/
 
-// TODO: Create a paragraph element using this document.createElement() and put this text inside "This node was created using the createElement() method"
+let e2Container = document.getElementById("parent") 
+let paragraph = document.createElement("p");
+let anchor = document.createElement("a");
+paragraph.textContent = 'This node was created using the createElement() method';
+anchor.textContent = "I am a <a> tag";
+anchor.href = "https://www.pinterest.com/";
+anchor.style.color = "white";
 
-// TODO: Append the created node to the parent node using the element.appendChild() method
 
-// TODO: Create a <a> element using this document.createElement() and put this text inside "I am a <a> tag"
-
-// BONUS: Add a link href to the <a>
-
-// TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
+e2Container.appendChild(paragraph);
+e2Container.appendChild(anchor);
+e2Container.insertBefore(anchor, paragraph);
 
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
 
-// TODO: Replace the "Child Node" with a new <p> element that reads "New Child Node"
+let e3Container = document.getElementById("exercise-container3");
+let oldChild = document.getElementById("N1");
+let newChild = document.createElement("p");
+newChild.textContent = "New Child Node";
 
-// TODO: Remove the "New Child Node"
+e3Container.appendChild(newChild);
+e3Container.replaceChild(newChild, oldChild);
+
+e3Container.removeChild(newChild);
 
 /*----------- Exercise #4: LIST ITEMS ----------- */
-// Use the following array of values to generate a list on the DOM
 
+let e4Container = document.getElementsByClassName("exercise4")[0];
 let list = [ "apples", "bananas", "carrots", "dragon fruit", "eggplant", "fish", "grapes", "honey", "ice bag", "juice (any kind)" ];
+let shoppingList = document.createElement("ul");
+let itemsSeparated = list.map((item) => {
+    let listItem = document.createElement("li");
+    listItem.textContent = item;
+    shoppingList.appendChild(listItem);
+    listItem.style.textAlign = "left";
+});
 
-
-// TODO: Create an unordered list element
-
-// TODO: Iterate over the array values, and create a list item element for each
-
-// TODO: Append the new list items to the unordered list element
-
-// TODO: Append the unordered list to the `div#container` under exercise 4 
+e4Container.appendChild(shoppingList);
+// shoppingList.appendChild(itemsSeparated);
 
 /*----------- Exercise #5: DOM EVENTS --------------*/
 
-// TODO: write a function called "show" which creates a new div with an alerting message to the user with this message
-// -> "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
-// This div should be a 'modal' that covers the main content on the screen
-// BONUS: The modal popup should be able to be closed. Refactor for this functionality
+const e5Container = document.getElementsByClassName("exercise5")[0];
+
+function show() {
+    let modal = document.createElement("div");
+    let btnDiv = document.createElement("div");
+    let modalTextContent = document.createElement("p");
+    let closeBtn = document.createElement("btn");
+    
+    modal.id = "modal";
+    btnDiv.className = "modal-card";
+    closeBtn.id = "btn";
+    modalTextContent.id = "modalTextContent";
+
+    e5Container.appendChild(modal);
+    modal.appendChild(btnDiv);
+    btnDiv.appendChild(modalTextContent);
+    btnDiv.appendChild(closeBtn);
+    
+    modalTextContent.textContent = "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
+    closeBtn.textContent = "Click to close";
+   
+    closeBtn.onclick = () => e5Container.removeChild(modal);
+}
+
+const btn = document.getElementById("btn");
+
+btn.onclick = show;
